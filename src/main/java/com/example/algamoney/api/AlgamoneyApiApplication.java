62a7396511861e80bdd390d9cc09c9f3e72080e1
ruriknj <ruriknj@hotmail.com>
2022-02-05 14:59:51 -0300

    Atualizado até o 6.5 Tópico: Configurando JWT no projeto

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -2,6 +2,9 @@ package com.example.algamoney.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @SpringBootApplication
 public class AlgamoneyApiApplication {
@@ -9,5 +12,12 @@ public class AlgamoneyApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
 	}
+	
+	@Bean
+    public HttpFirewall allowUrlSemicolonHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);
+        return firewall;
+    }
 
 }
